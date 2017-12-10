c1de70f82957e0a6b5f99017a8832940fbb0a313
Melisa Im <melisaevelynim@gmail.com>
2017-12-09 16:29:27 -0800

    Add basic user info box

--- src/components/profile/ProfileInfoComponent.js
+++ src/components/profile/ProfileInfoComponent.js
@@ -1,19 +1,43 @@
 import React, { PureComponent } from "react";
-import { Header, Image } from "semantic-ui-react";
+import { Header, Image, Segment } from "semantic-ui-react";
 
 export default class ProfileInfoComponent extends PureComponent {
 	state = {};
 
 	render() {
 		return (
-			<div>
-				<Header as="h2">
+			<div
+				style={{
+					display: "flex",
+					borderBottom: "solid black 10px",
+					borderTop: "solid black 10px"
+				}}>
+				<div>
 					<Image
 						circular
-						src="https://lh3.googleusercontent.com/dB3Dvgf3VIglusoGJAfpNUAANhTXW8K9mvIsiIPkhJUAbAKGKJcEMPTf0mkSexzLM5o=w300"
+						size="large"
+						style={{ padding: "10px", height: "180px", width: "220px" }}
+						src="https://news.nationalgeographic.com/content/dam/news/photos/000/755/75552.ngsversion.1422285553360.adapt.1900.1.jpg"
 					/>
-					{this.props.info.name}
-				</Header>
+					<div
+						style={{
+							display: "flex",
+							justifyContent: "center",
+							fontWeight: "bold",
+							paddingBottom: "5px"
+						}}>
+						{this.props.info.name}
+					</div>
+				</div>
+				<div
+					style={{
+						width: "100%",
+						display: "flex",
+						justifyContent: "center",
+						alignItems: "center"
+					}}>
+					{this.props.info.name} is a {this.props.info.age} year old {this.props.info.gender}.
+				</div>
 			</div>
 		);
 	}
